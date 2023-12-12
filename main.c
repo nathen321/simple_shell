a686e21e71eeccac902c9f6f70049ea6b1cfcad5
nathen321 <walidjalaly43@gmail.com>
2023-12-12 14:54:33 -0800

    the programme no longer crash with empty input

--- main.c
+++ main.c
@@ -1,38 +1,4 @@
 #include "main.h"
-/**
- * user_input - take input from the user
- * @are: array of string
- * Return: ....
- */
-void user_input(char *are[])
-{
-	unsigned int i = 0;
-	char *buffer;
-	size_t bufsize = 32;
-	char *token;
-	const char s[2] = " ";
-	int size;
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-	getline(&buffer, &bufsize, stdin);
-
-	size = strlen(buffer);
-	buffer[size - 1] = '\0';
-
-	token = strtok(buffer, s);
-
-	while (token != NULL)
-	{
-		are[i] = token;
-		token = strtok(NULL, s);
-		i++;
-	}
-}
 
 /**
  * reset_bauf - reset de command buffer
@@ -46,6 +12,49 @@ void reset_bauf(char *are[])
 	while (are[i] != NULL)
 	{
 		are[i] = NULL;
+		i++;
+	}
+}
+
+/**
+ * user_input - take input from the user
+ * @are: array of string
+ * Return: ....
+ */
+void user_input(char *are[])
+{
+	unsigned int i = 0;
+	char *buffer;
+	size_t bufsize = 32;
+	char *token;
+	const char s[2] = " ";
+	int size;
+	ssize_t chars_read;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+
+	chars_read = getline(&buffer, &bufsize, stdin);
+
+	size = strlen(buffer);
+	buffer[size - 1] = '\0';
+	token = strtok(buffer, s);
+
+	if (chars_read == 1)
+	{
+		reset_bauf(are);
+		return;
+	}
+
+	while (token != NULL)
+	{
+		are[i] = token;
+		token = strtok(NULL, s);
 		i++;
 	}
 }
@@ -66,7 +75,8 @@ int main(void)
 
 		user_input(buffer);
 
-		execut_command(buffer);
+		if (buffer[0] != NULL)
+			execut_command(buffer);
 
 		reset_bauf(buffer);
 	}
